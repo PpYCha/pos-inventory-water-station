45119c16a5a4bbb315f12bdf7281c4338b132cd4
PpYCha <diasandiaz7@gmail.com>
2022-09-19 21:45:42 +0800

    Login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard/Dashboard";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="dashboard/*" element={<Dashboard />}>
             {/* <Route path="/" element={<Dashboard />} /> */}
           </Route>
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>LOGIN INE KUNTA INE</div>;
+};
+
+export default Home;
